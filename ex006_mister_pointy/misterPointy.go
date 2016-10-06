a526eeb118a22546374325203b69faf882d18312
Chris Handley <chrisrhandley@gmail.com>
2016-10-06 21:55:43 +0100

    solve mr pointy

--- ex006_mister_pointy/misterPointy.go
+++ ex006_mister_pointy/misterPointy.go
@@ -1,19 +1,28 @@
 package misterPointy
 
-func add(a, b) {
+func add(a, b int) int {
+	return a + b
 }
 
-func addPointers(a, b) {
+func addPointers(a, b *int) int {
+	return *a + *b
 }
 
-func addReturnPointer(a, b) {
+func addReturnPointer(a, b int) *int{
+	result := a + b
+	return &result
 }
 
-func addPointersReturnPointer(a, b) {
+func addPointersReturnPointer(a, b *int) *int {
+	result := *a + *b
+	return &result
 }
 
-func addModifyArgument(a, b) {
+func addModifyArgument(a int, b *int) *int {
+	*b = a + *b
+	return b
 }
 
-func addPointerToPointer(a, b) {
+func addPointerToPointer(a, b **int) int {
+	return **a + **b
 }
