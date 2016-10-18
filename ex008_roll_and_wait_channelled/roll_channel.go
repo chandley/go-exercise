c144972e974b1a14468b5ecf1c2476aa4fd9303c
Chris Handley <chrisrhandley@gmail.com>
2016-10-18 16:08:31 +0100

    ex008 for 1m players

--- ex008_roll_and_wait_channelled/roll_channel.go
+++ ex008_roll_and_wait_channelled/roll_channel.go
@@ -4,6 +4,7 @@ import "time"
 import "fmt"
 import "math/rand"
 import "sort"
+import "strconv"
 
 type player struct {
 	name  string
@@ -18,23 +19,40 @@ type roll struct {
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for _ = range ticker.C {
+			fmt.Printf(".")
+		}
+	}()
+
 	gameTimer := make(chan string)
 	go func() {
-		time.Sleep(10 * time.Second)
+		time.Sleep(15 * time.Second)
 		gameTimer <- "Time is up!"
 	}()
 
-	players := []player{player{name: "Sid"}, player{name: "Nancy"}, player{name: "Gavin"}, player{name: "Tracey"}}
+	players := []player{}
+
+	for i := 0; i < 1000000; i++ {
+		players = append(players, player{name: "player " + strconv.Itoa(i)})
+	}
+
+
+
+	//players := []player{player{name: "Sid"}, player{name: "Nancy"}, player{name: "Gavin"}, player{name: "Tracey"}}
 	for index := range players {
 		go tally(&players[index], keepRolling())
 	}
 
 	fmt.Println(<-gameTimer)
 
-	sort.Sort(sort.Reverse(ByTotal(players)))
-	for _, finisher := range players {
+	sort.Sort(ByTotal(players))
+	showFrom := len(players) - 100
+	for _, finisher := range players[showFrom:] {
 		fmt.Printf("%v scored total %v\n", finisher.name, finisher.total)
 	}
+	sort.Sort(sort.Reverse(ByTotal(players)))
 	if players[0].total == players[1].total {
 		fmt.Printf("It was a draw")
 	} else {
@@ -68,7 +86,7 @@ func tally(player *player, channel chan roll) {
 	for {
 		roll := <-channel
 		player.total += roll.score
-		fmt.Printf("%v (%v) rolled %v \n", player.name, player.total, roll.message)
+	//	fmt.Printf("%v (%v) rolled %v \n", player.name, player.total, roll.message)
 	}
 }
 
